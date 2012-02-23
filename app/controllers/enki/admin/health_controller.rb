9b225fbfe7450ddedf2e9e662e186899cacbb757
Andy Triggs <andy.triggs@gmail.com>
2012-02-23 17:07:59 +0000

    Namespaced admin constrollers and inherited from Enki::Admin::BaseController.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- app/controllers/enki/admin/health_controller.rb
+++ app/controllers/enki/admin/health_controller.rb
@@ -1,19 +1,21 @@
 module Enki
-  class Admin::HealthController < Admin::BaseController
-    before_filter :require_login
-    verify :method => 'post',
-           :only   => 'generate_exception',
-           :add_headers => {
-             "Allow" => "POST"},
-           :render => {
-             :text   => 'Method not allowed',
-             :status => 405}
+  module Admin
+    class HealthController < BaseController
+      before_filter :require_login
+      verify :method => 'post',
+             :only   => 'generate_exception',
+             :add_headers => {
+               "Allow" => "POST"},
+             :render => {
+               :text   => 'Method not allowed',
+               :status => 405}
 
-    def index
-    end
+      def index
+      end
 
-    def generate_exception
-      raise RuntimeError
+      def generate_exception
+        raise RuntimeError
+      end
     end
   end
 end
--- app/controllers/enki/admin/undo_items_controller.rb
+++ app/controllers/enki/admin/undo_items_controller.rb
@@ -1,39 +1,41 @@
 module Enki
-  class Admin::UndoItemsController < Admin::BaseController
-    def index
-      @undo_items = UndoItem.find(:all,
-        :order => 'created_at DESC',
-        :limit => 50
-      )
-    end
+  module Admin
+    class UndoItemsController < BaseController
+      def index
+        @undo_items = UndoItem.find(:all,
+          :order => 'created_at DESC',
+          :limit => 50
+        )
+      end
 
-    def undo
-      item = UndoItem.find(params[:id])
-      begin
-        object = item.process!
+      def undo
+        item = UndoItem.find(params[:id])
+        begin
+          object = item.process!
 
-        respond_to do |format|
-          format.html {
-            flash[:notice] = item.complete_description
-            redirect_to(:back)
-          }
-          format.json {
-            render :json => {
-              :message => item.complete_description,
-              :obj     => object.attributes
+          respond_to do |format|
+            format.html {
+              flash[:notice] = item.complete_description
+              redirect_to(:back)
             }
-          }
-        end
-      rescue UndoFailed
-        msg = "Could not undo, would result in an invalid state (i.e. a comment with no post)"
-        respond_to do |format|
-          format.html {
-            flash[:notice] = msg
-            redirect_to(:back)
-          }
-          format.json {
-            render :json => { :message => msg }
-          }
+            format.json {
+              render :json => {
+                :message => item.complete_description,
+                :obj     => object.attributes
+              }
+            }
+          end
+        rescue UndoFailed
+          msg = "Could not undo, would result in an invalid state (i.e. a comment with no post)"
+          respond_to do |format|
+            format.html {
+              flash[:notice] = msg
+              redirect_to(:back)
+            }
+            format.json {
+              render :json => { :message => msg }
+            }
+          end
         end
       end
     end
