25cef42c50bc3160dba606f963e0d3bd14548332
Andy Triggs <andy.triggs@gmail.com>
2012-02-23 17:29:03 +0000

    Edited the routes file for Enki namespace, and to draw them in the engine router.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,36 @@
-Enki::Application.routes.draw do
-  namespace :admin do
-    resource :session
+Enki::Engine.routes.draw do
+  scope :module => 'enki' do
+    namespace :admin do 
+      resource :session
 
-    resources :posts, :pages do
-      post 'preview', :on => :collection
+      resources :posts, :pages do
+        post 'preview', :on => :collection
+      end
+      resources :comments
+      resources :undo_items do
+        post 'undo', :on => :member
+      end
+
+      match 'health(/:action)' => 'health', :action => 'index', :as => :health
+
+      root :to => 'dashboard#show'
     end
-    resources :comments
-    resources :undo_items do
-      post 'undo', :on => :member
-    end
-
-    match 'health(/:action)' => 'health', :action => 'index', :as => :health
-
-    root :to => 'dashboard#show'
+    resources :archives, :only => [:index]
+    resources :pages, :only => [:show]
   end
 
-  resources :archives, :only => [:index]
-  resources :pages, :only => [:show]
-
+  
   constraints :year => /\d{4}/, :month => /\d{2}/, :day => /\d{2}/ do
-    get ':year/:month/:day/:slug/comments'  => 'comments#index'
-    post ':year/:month/:day/:slug/comments' => 'comments#create'
-    get ':year/:month/:day/:slug/comments/new' => 'comments#new'
-    get ':year/:month/:day/:slug' => 'posts#show'
+    #get ':year/:month/:day/:slug/comments'  => 'enki/comments#index'
+    #post ':year/:month/:day/:slug/comments' => 'enki/comments#create'
+    #get ':year/:month/:day/:slug/comments/new' => 'enki/comments#new'
+    get ':year/:month/:day/:slug' => 'enki/posts#show'
   end
 
-  scope :to => 'posts#index' do
+  scope :to => 'enki/posts#index' do
     get 'posts.:format', :as => :formatted_posts
-    get '(:tag)', :as => :posts
+    get 'tags/(:tag)', :as => :posts
   end
 
-  root :to => 'posts#index'
+  root :to => 'enki/posts#index'
 end
