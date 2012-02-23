e69bc07d478506f74f620107a5b373e3ca7b41f8
Andy Triggs <andy.triggs@gmail.com>
2012-02-23 17:14:56 +0000

    Removed obsolete initializers.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- config/initializers/exception_notifier.rb
+++
@@ -1,7 +0,0 @@
-require 'action_controller/metal/request_forgery_protection'
-
-Enki::Application.config.middleware.use ExceptionNotifier,
-  :ignore_exceptions    => [ActionController::InvalidAuthenticityToken],
-  :email_prefix         => "[Enki] ",
-  :sender_address       => [Enki::Config.default[:author, :email]],
-  :exception_recipients => [Enki::Config.default[:author, :email]]
