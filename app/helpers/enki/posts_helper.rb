5dde286a5b76a20b148e73301cb50060906828b5
Andy Triggs <andy.triggs@gmail.com>
2012-02-23 17:13:09 +0000

    Namespaced helpers properly.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- app/helpers/enki/posts_helper.rb
+++ app/helpers/enki/posts_helper.rb
@@ -1,7 +1,9 @@
-module Enki::PostsHelper
-  # This isn't strictly correct, but it's a pretty good guess
-  # and saves another hit to the DB
-  def more_content?
-    @posts.size == Post::DEFAULT_LIMIT
+module Enki
+  module PostsHelper
+    # This isn't strictly correct, but it's a pretty good guess
+    # and saves another hit to the DB
+    def more_content?
+      @posts.size == Post::DEFAULT_LIMIT
+    end
   end
 end
--- app/helpers/enki/tag_helper.rb
+++ app/helpers/enki/tag_helper.rb
@@ -1,5 +1,7 @@
-module Enki::TagHelper
-  def linked_tag_list(tags)
-    raw tags.collect {|tag| link_to(tag.name, posts_path(:tag => tag.name))}.join(", ")
+module Enki
+  module TagHelper
+    def linked_tag_list(tags)
+      raw tags.collect {|tag| link_to(tag.name, enki.posts_path(:tag => tag.name))}.join(", ")
+    end
   end
 end
