e6a0a1c1ccae06a93a9d2510f12c19b2c9c32c10
Andy Triggs <andy.triggs@gmail.com>
2012-02-23 17:27:10 +0000

    Added a simple seeds file, primarily for pagination testing.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+if Rails.env == 'development'
+  module Enki
+    Post.destroy_all
+    20.times do |idx|
+      Post.create!(:title => "Test post #{idx}", :body => "Lorem ipsum plipsum nipsum.")
+    end
+  end
+end
