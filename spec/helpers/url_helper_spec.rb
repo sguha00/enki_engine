663dcce7fe3aff012f1aa7b9c3e87637410ab909
Andy Triggs <andy.triggs@gmail.com>
2012-02-29 18:05:38 +0000

    Fixed up helper specs.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- spec/helpers/url_helper_spec.rb
+++ spec/helpers/url_helper_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
 
-describe UrlHelper do
-  include UrlHelper
+module Enki
+  describe UrlHelper do
+    include UrlHelper
 
-  describe '#post_path' do
-    it 'should prefix slug with published_at' do
-      post = stub(
-        :published_at => Date.new(2012,1,1),
-        :slug         => 'post'
-      )
-      post_path(post).should == '/2012/01/01/post'
+    def enki_config
+      config = double('config')
+      config.should_receive('[]').with(:engine, :mount_at).and_return('')
+      config
+    end
+
+    describe '#post_path' do
+      it 'should prefix slug with published_at' do
+        post = stub(
+          :published_at => Date.new(2012,1,1),
+          :slug         => 'post'
+        )
+        post_path(post).should == '/2012/01/01/post'
+      end
     end
   end
 end
