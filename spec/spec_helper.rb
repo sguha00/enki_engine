62990184f789cf8622205c0c055e82b61a52f44c
Andy Triggs <andy.triggs@gmail.com>
2012-02-29 18:06:27 +0000

    Spec helper changes for testing as an engine.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,17 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+
+# See http://reinteractive.net/posts/2-start-your-engines for useful info regarding testing Engines.
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
+
+ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].each {|f| require f }
+
+ENKI_CONFIG = Enki::Config.new(File.expand_path("../dummy/config/enki.yml",  __FILE__))
 
 RSpec.configure do |config|
   # == Mock Framework
