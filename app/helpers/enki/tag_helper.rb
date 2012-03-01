0d72a71beb79967fcb81eb9c38076e8b84717633
Andy Triggs <andy.triggs@gmail.com>
2012-03-01 11:03:55 +0000

    Added options argument to linked_tag_list.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- app/helpers/enki/tag_helper.rb
+++ app/helpers/enki/tag_helper.rb
@@ -1,7 +1,7 @@
 module Enki
   module TagHelper
-    def linked_tag_list(tags)
-      raw tags.collect {|tag| link_to(tag.name, enki.posts_path(:tag => tag.name))}.join(", ")
+    def linked_tag_list(tags, options = {})
+      raw tags.collect {|tag| link_to(tag.name, enki.posts_path(:tag => tag.name), options)}.join(", ")
     end
   end
 end
