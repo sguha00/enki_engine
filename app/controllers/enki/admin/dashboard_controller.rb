2d4756f91b9e798b4dacb7e3b8fe7d43c1b0466d
Andy Triggs <andy.triggs@gmail.com>
2012-03-07 11:08:58 +0000

    Added pages to dashboard action.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- app/controllers/enki/admin/dashboard_controller.rb
+++ app/controllers/enki/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ module Enki
     class DashboardController < BaseController
       def show
         @posts            = Post.find_recent(:limit => 8)
+        @pages            = Page.limit(8)
         @stats            = Stats.new
       end
     end
