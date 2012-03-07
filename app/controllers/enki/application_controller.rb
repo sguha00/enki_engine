25be311fa72a6312b0d5b60b378da6972dbcf959
Andy Triggs <andy.triggs@gmail.com>
2012-03-07 17:50:12 +0000

    Removed enki_config references and replaced with Enki.config.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- app/controllers/enki/application_controller.rb
+++ app/controllers/enki/application_controller.rb
@@ -12,22 +12,5 @@ module Enki
     include TagHelper
     include UrlHelper
 
-    protected
-
-    def enki_config
-      @@enki_config ||= Enki::Config.default
-    end
-    helper_method :enki_config
-
-    def comments?
-      enki_config[:features, :comments]
-    end
-    helper_method :comments?
-
-    def tags?
-      enki_config[:features, :tags]
-    end
-    helper_method :tags?
-    
   end
 end
