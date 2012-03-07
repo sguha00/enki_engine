f9abd0da71631272debc4f26cd4226b1e3434c30
Andy Triggs <andy.triggs@gmail.com>
2012-03-07 17:51:13 +0000

    Namespaced remaining models.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- app/models/enki/delete_post_undo.rb
+++ app/models/enki/delete_post_undo.rb
@@ -1,34 +1,36 @@
-class Enki::DeletePostUndo < UndoItem
-  def process!
-    post_attributes = loaded_data[:post]
-    raise('Post already exists') if Post.find_by_id(post_attributes.delete('id').to_i)
+module Enki
+  class DeletePostUndo < UndoItem
+    def process!
+      post_attributes = loaded_data[:post]
+      raise('Post already exists') if Post.find_by_id(post_attributes.delete('id').to_i)
 
-    post = nil
-    transaction do
-      post = Post.create!(post_attributes)
-      loaded_data[:comments].each do |comment|
-        post.comments.create!(comment.except('id', 'post_id'))
+      post = nil
+      transaction do
+        post = Post.create!(post_attributes)
+        loaded_data[:comments].each do |comment|
+          post.comments.create!(comment.except('id', 'post_id'))
+        end
+        self.destroy
       end
-      self.destroy
+      post
     end
-    post
-  end
 
-  def loaded_data
-    @loaded_data ||= YAML.load(data)
-  end
+    def loaded_data
+      @loaded_data ||= YAML.load(data)
+    end
 
-  def description
-    "Deleted post '#{loaded_data[:post]["title"]}'"
-  end
+    def description
+      "Deleted post '#{loaded_data[:post]["title"]}'"
+    end
 
-  def complete_description
-    "Recreated post '#{loaded_data[:post]["title"]}'"
-  end
+    def complete_description
+      "Recreated post '#{loaded_data[:post]["title"]}'"
+    end
 
-  class << self
-    def create_undo(post)
-      DeletePostUndo.create!(:data => {:post => post.attributes, :comments => post.comments.collect(&:attributes)}.to_yaml)
+    class << self
+      def create_undo(post)
+        DeletePostUndo.create!(:data => {:post => post.attributes, :comments => post.comments.collect(&:attributes)}.to_yaml)
+      end
     end
   end
 end
