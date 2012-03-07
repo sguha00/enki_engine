f567be7cdf8d45749eaefa4c4c00db6952b69a01
Andy Triggs <andy.triggs@gmail.com>
2012-03-07 11:57:02 +0000

    Fixed indenting on helper method.
    Signed-off-by: Andy Triggs <andy.triggs@gmail.com>

--- app/helpers/enki/application_helper.rb
+++ app/helpers/enki/application_helper.rb
@@ -19,14 +19,14 @@ module Enki
       }[error.first.to_s]
     end
 
-  # A simplistic way of skipping the host app view paths and rendering a file directly from a Rails::Engine. 
-  # An engine can technically have multiple app/views paths. This method uses only the first defined one.
-  # 
-  # TODO: Add support for rendering methods other than :file.
-  def render_enki(options)
-    raise ArgumentError, 'This method only supports file rendering currently.' unless options[:file]
-    render options.merge(:file => File.join(Enki::Engine.paths['app/views'].first.to_s, options[:file]))
-  end
+    # A simplistic way of skipping the host app view paths and rendering a file directly from a Rails::Engine. 
+    # An engine can technically have multiple app/views paths. This method uses only the first defined one.
+    # 
+    # TODO: Add support for rendering methods other than :file.
+    def render_enki(options)
+      raise ArgumentError, 'This method only supports file rendering currently.' unless options[:file]
+      render options.merge(:file => File.join(Enki::Engine.paths['app/views'].first.to_s, options[:file]))
+    end
   
   end
 end
