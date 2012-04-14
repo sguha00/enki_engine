304d2dba30c083da311404828a9700eb9d3b9e96
James McCarthy <james2mccarthy@gmail.com>
2012-04-15 00:25:28 +0100

    Ensured DashboardController checks for comments and returns if they are enabled.
    Signed-off-by: James McCarthy <james2mccarthy@gmail.com>

--- app/controllers/enki/admin/dashboard_controller.rb
+++ app/controllers/enki/admin/dashboard_controller.rb
@@ -4,6 +4,7 @@ module Enki
       def show
         @posts            = Post.find_recent(:limit => 8)
         @pages            = Page.limit(8)
+        @comment_activity = CommentActivity.find_recent if comments?
         @stats            = Stats.new
       end
     end
