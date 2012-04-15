eba48d2bd7f5d0d3c2c0368da0ac256d64ff2247
James McCarthy <james2mccarthy@gmail.com>
2012-04-15 19:34:00 +0100

    Added a paginated helper shim for will_paginate and kaminari in views.
    Signed-off-by: James McCarthy <james2mccarthy@gmail.com>

--- app/helpers/enki/application_helper.rb
+++ app/helpers/enki/application_helper.rb
@@ -5,13 +5,6 @@ module Enki
       Struct.new(:name, :email).new(Enki.config[:author][:name], Enki.config[:author][:email])
     end
 
-    def open_id_delegation_link_tags(server, delegate)
-      raw links = <<-EOS
-        <link rel="openid.server" href="#{server}">
-        <link rel="openid.delegate" href="#{delegate}">
-      EOS
-    end
-
     def format_comment_error(error)
       {
         'body'   => 'Please comment',
@@ -26,6 +19,16 @@ module Enki
     
     def tags?
       Enki.config.tags?
+    end
+    
+    def paginated(*args)
+      if defined? ::Kaminari
+        paginate(*args)
+      elsif defined? ::WillPagniate
+        will_paginate(*args)
+      else
+        nil
+      end
     end
 
   end
