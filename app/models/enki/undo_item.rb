898030d77923037d9b954638c9082a61c2a30991
James McCarthy <james2mccarthy@gmail.com>
2012-04-15 01:51:42 +0100

    Added attr_accessible to UndoItem.
    Signed-off-by: James McCarthy <james2mccarthy@gmail.com>

--- app/models/enki/undo_item.rb
+++ app/models/enki/undo_item.rb
@@ -1,5 +1,8 @@
 module Enki
   class UndoItem < ActiveRecord::Base
+    
+    attr_accessible :data, :type
+    
     def process!
       raise("#process must be implemented by subclasses")
     end
