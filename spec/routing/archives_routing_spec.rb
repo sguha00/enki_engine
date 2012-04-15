ba8e982b19bf5e27cb2ef11a7540a8ee2df6b9a6
James McCarthy <james2mccarthy@gmail.com>
2012-04-15 14:04:10 +0100

    Fixed up archives_controller_spec and archives_routing_spec.
    Signed-off-by: James McCarthy <james2mccarthy@gmail.com>

--- spec/routing/archives_routing_spec.rb
+++ spec/routing/archives_routing_spec.rb
@@ -1,9 +1,9 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Enki::ArchivesController do
-  describe 'route' do
-    it 'generates index params' do
-      {:get => '/archives'}.should route_to(:controller => 'archives', :action => 'index')
-    end
+describe 'Enki::ArchivesController routes' do
+
+  it 'routes get /archives to archives#index' do
+    get('/archives').should route_to('enki/archives#index')
   end
+
 end
