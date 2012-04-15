ecd6ebc6395e4e6003158215656b9838fe34076b
James McCarthy <james2mccarthy@gmail.com>
2012-04-15 15:53:48 +0100

    Fixed up some more routes and route specs.
    Signed-off-by: James McCarthy <james2mccarthy@gmail.com>

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'PostsController routes' do
   
   it "should route /code to posts#index with tag code" do
-    get("/tags/code").should route_to("enki/posts#index", :tag => 'code')
+    get("/code").should route_to("enki/posts#index", :tag => 'code')
   end
     
   it "should route /posts.atmo to posts#index format atom" do
@@ -11,15 +11,15 @@ describe 'PostsController routes' do
   end
     
   it "should route /pages to posts#index with tag pages" do
-    get("/pages").should route_to(:controller => 'enki/posts', :action => 'index', :tag => 'pages')
+    get("/pages").should route_to('enki/posts#index', :tag => 'pages')
   end
     
   it "should route /code.atmo to posts#index tag: code and format: atom" do
-    {:get => "/tags/code.atom"}.should route_to("enki/posts#index", :tag => "code", :format => 'atom')
+    get("/code.atom").should route_to("enki/posts#index", :tag => "code", :format => 'atom')
   end
 
   it "should route /2008/02/01/a-post to posts#show with year: 2008, month: 02, day: 01, slug: a-post" do
-    {:get => "/2008/02/01/a-post"}.should route_to("enki/posts#show", :year => '2008', :month => '02', :day => '01', :slug => 'a-post')
+    get("/2008/02/01/a-post").should route_to("enki/posts#show", :year => '2008', :month => '02', :day => '01', :slug => 'a-post')
   end
 
 end
