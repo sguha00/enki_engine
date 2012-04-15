8d6d7dd1cc9106ce61bdf2dd57f9a1f182919fa4
James McCarthy <james2mccarthy@gmail.com>
2012-04-15 01:46:08 +0100

    Updated admin/health_controller_spec.
    Signed-off-by: James McCarthy <james2mccarthy@gmail.com>

--- spec/controllers/admin/health_controller_spec.rb
+++ spec/controllers/admin/health_controller_spec.rb
@@ -1,46 +1,49 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe Admin::HealthController do
-  describe 'handling GET to index' do
-    before(:each) do
-      session[:logged_in] = true
-      get :index
-    end
+module Enki
 
-    it "is successful" do
-      response.should be_success
-    end
+  describe Admin::HealthController do
+    describe 'handling GET to index' do
+      before(:each) do
+        controller.stub!(:logged_in?).and_return(true)
+        get :index
+      end
 
-    it "renders health template" do
-      response.should render_template("index")
-    end
-  end
+      it "is successful" do
+        response.should be_success
+      end
 
-  describe 'handling POST to generate_exception' do
-    describe 'when logged in' do
-      it 'raises a RuntimeError' do
-        session[:logged_in] = true
-        lambda {
-          post :generate_exception
-        }.should raise_error
+      it "renders health template" do
+        response.should render_template("index")
       end
     end
 
-    describe 'when not logged in' do
-      it 'does not raise' do
-        lambda {
-          post :generate_exception
-        }.should_not raise_error
+    describe 'handling POST to generate_exception' do
+      describe 'when logged in' do
+        it 'raises a RuntimeError' do
+          controller.stub!(:logged_in?).and_return(true)
+          lambda {
+            post :generate_exception
+          }.should raise_error
+        end
+      end
+
+      describe 'when not logged in' do
+        it 'does not raise' do
+          lambda {
+            post :generate_exception
+          }.should raise_error(Enki::NotLoggedInError)
+        end
       end
     end
-  end
 
-  describe 'handling GET to generate_exception' do
-    it '405s' do
-      session[:logged_in] = true
-      get :generate_exception
-      response.status.should == 405
-      response.headers['Allow'].should == 'POST'
+    describe 'handling GET to generate_exception' do
+      it '405s' do
+        controller.stub!(:logged_in?).and_return(true)
+        get :generate_exception
+        response.status.should == 405
+        response.headers['Allow'].should == 'POST'
+      end
     end
   end
 end
